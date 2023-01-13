1af7c84fea18428e4d845bf194cd8a36d75d4a69
adithya <madithyam16@gmail.com>
2023-01-13 19:46:55 +0530

    Logger

--- src/main/java/adithyaproj/server/customer/enitity/Department.java
+++ src/main/java/adithyaproj/server/customer/enitity/Department.java
@@ -5,9 +5,17 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Department {
 
     @Id
@@ -19,53 +27,56 @@ public class Department {
     private String departmentAddress;
     private String departmentCode;
 
-    @Override
-    public String toString() {
-        return "Department [departmentId=" + departmentId + ", departmentName=" + departmentName
-                + ", departmentAddress=" + departmentAddress + ", departmentCode=" + departmentCode + "]";
-    }
+    // @Override
+    // public String toString() {
+    // return "Department [departmentId=" + departmentId + ", departmentName=" +
+    // departmentName
+    // + ", departmentAddress=" + departmentAddress + ", departmentCode=" +
+    // departmentCode + "]";
+    // }
 
-    public Long getDepartmentId() {
-        return departmentId;
-    }
+    // public Long getDepartmentId() {
+    // return departmentId;
+    // }
 
-    public Department(Long departmentId, String departmentName, String departmentAddress, String departmentCode) {
-        this.departmentId = departmentId;
-        this.departmentName = departmentName;
-        this.departmentAddress = departmentAddress;
-        this.departmentCode = departmentCode;
-    }
+    // public Department(Long departmentId, String departmentName, String
+    // departmentAddress, String departmentCode) {
+    // this.departmentId = departmentId;
+    // this.departmentName = departmentName;
+    // this.departmentAddress = departmentAddress;
+    // this.departmentCode = departmentCode;
+    // }
 
-    public void setDepartmentId(Long departmentId) {
-        this.departmentId = departmentId;
-    }
+    // public void setDepartmentId(Long departmentId) {
+    // this.departmentId = departmentId;
+    // }
 
-    public String getDepartmentName() {
-        return departmentName;
-    }
+    // public String getDepartmentName() {
+    // return departmentName;
+    // }
 
-    public void setDepartmentName(String departmentName) {
-        this.departmentName = departmentName;
-    }
+    // public void setDepartmentName(String departmentName) {
+    // this.departmentName = departmentName;
+    // }
 
-    public String getDepartmentAddress() {
-        return departmentAddress;
-    }
+    // public String getDepartmentAddress() {
+    // return departmentAddress;
+    // }
 
-    public void setDepartmentAddress(String departmentAddress) {
-        this.departmentAddress = departmentAddress;
-    }
+    // public void setDepartmentAddress(String departmentAddress) {
+    // this.departmentAddress = departmentAddress;
+    // }
 
-    public String getDepartmentCode() {
-        return departmentCode;
-    }
+    // public String getDepartmentCode() {
+    // return departmentCode;
+    // }
 
-    public void setDepartmentCode(String departmentCode) {
-        this.departmentCode = departmentCode;
-    }
+    // public void setDepartmentCode(String departmentCode) {
+    // this.departmentCode = departmentCode;
+    // }
 
-    public Department() {
+    // public Department() {
 
-    }
+    // }
 
 }
